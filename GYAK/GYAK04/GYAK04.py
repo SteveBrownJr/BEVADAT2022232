c51831401bdc6f1f5955dc602305d62bd4f9f310
Steve_Brown <barnapisti19671@gmail.com>
2023-03-24 11:08:30 +0100

    Improvement in the function 4 and 6

--- GYAK/GYAK04/GYAK04.py
+++ GYAK/GYAK04/GYAK04.py
@@ -18,7 +18,7 @@ stats = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
        "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
        "area": [8.516, 17.10, 3.286, 9.597, 1.221],
        "population": [200.4, 143.5, 1252, 1357, 52.98] }
-
+#1
 def dict_to_dataframe(test_dict : dict = stats) -> pd.DataFrame:
     test_df = pd.DataFrame(test_dict)
     return test_df
@@ -33,7 +33,7 @@ Egy példa a kimenetre: test_df
 return type: pandas.core.series.Series
 függvény neve: get_column
 '''
-
+#2
 def get_column(test_df : pd.DataFrame = dict_to_dataframe(), area : str = "") -> pd.Series:
     new_df = test_df.copy()
     ret = new_df[area]
@@ -47,7 +47,7 @@ Egy példa a kimenetre: test_df
 return type: pandas.core.frame.DataFrame
 függvény neve: get_top_two
 '''
-
+#3
 def get_top_two(test_df : pd.DataFrame = dict_to_dataframe())-> pd.DataFrame:
     df = test_df.copy()
     ret = df.nlargest(2, "area")
@@ -62,9 +62,9 @@ Egy példa a kimenetre: test_df
 return type: pandas.core.frame.DataFrame
 függvény neve: population_density
 '''
-
+#4?
 def population_density(test_df : pd.DataFrame = dict_to_dataframe()) -> pd.DataFrame:
-    df = test_df.copy
+    df = test_df.copy()
     df["density"] = df["population"] / df["area"]
     return df
 
@@ -82,7 +82,7 @@ Egy példa a kimenetre: fig
 return type: matplotlib.figure.Figure
 függvény neve: plot_population
 '''
-
+#5
 def plot_population(test_df : pd.DataFrame = dict_to_dataframe())-> plt.Figure:
     df = test_df.copy()
     fig, ax = plt.subplots()
@@ -105,13 +105,13 @@ Egy példa a kimenetre: fig
 return type: matplotlib.figure.Figure
 függvény neve: plot_area
 '''
-
+#6?
 def plot_area(test_df : pd.DataFrame = dict_to_dataframe()) -> plt.Figure:
     df = test_df.copy()
-    sizes = df['area']
-    labels = df['country']
     fig1, ax1 = plt.subplots()
     ax1.set_title("Area of Countries")
-    ax1.pie(sizes, labels=labels, startangle=90)
+    ax1.pie(df['area'], labels=df['country'])
     ax1.axis('equal')
     return fig1
+plot_area().show()
+input()
