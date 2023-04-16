fa78e429cd42ba2b1cead9ae6532014762dbf1fb
Steve_Brown <barnapisti19671@gmail.com>
2023-04-16 13:33:57 +0200

    GYAK06 Solution added

--- HAZI/HAZI06/HAZI06.py
+++ HAZI/HAZI06/HAZI06.py
@@ -1,29 +1,31 @@
 """
-1. Értelmezd az adatokat!!!
+1.  Értelmezd az adatokat!!!
+    A feladat megoldásához használd a NJ transit + Amtrack csv-t a moodle-ból.
+    A NJ-60k az a megoldott. Azt fogom használni a modellek teszteléséhez, illetve össze tudod hasonlítani az eredményedet.    
 
 2. Írj egy osztályt a következő feladatokra:  
-     - Neve legyen NJCleaner és mentsd el a NJCleaner.py-ba. Ebben a fájlban csak ez az osztály legyen.
-     - Konsturktorban kapja meg a csv elérési útvonalát és olvassa be pandas segítségével és mentsük el a data (self.data) osztályszintű változóba 
-     - Írj egy függvényt ami sorbarendezi a dataframe-et 'scheduled_time' szerint növekvőbe és visszatér a sorbarendezett df-el, a függvény neve legyen 'order_by_scheduled_time' és térjen vissza a df-el  
-     - Dobjuk el a from és a to oszlopokat, illetve a nan-okat és adjuk vissza a df-et. A függvény neve legyen 'drop_columns_and_nan' és térjen vissza a df-el  
-     - A date-et alakítsd át napokra, pl.: 2018-03-01 --> Thursday, ennek az oszlopnak legyen neve a 'day'. Ezután dobd el a 'date' oszlopot és térjen vissza a df-el. A függvény neve legyen 'convert_date_to_day' és térjen vissza a df-el   
-     - Hozz létre egy új oszlopot 'part_of_the_day' névvel. A 'scheduled_time' oszlopból számítsd ki az alábbi értékeit. A 'scheduled_time'-ot dobd el. A függvény neve legyen 'convert_scheduled_time_to_part_of_the_day' és térjen vissza a df-el  
+     2.1 Neve legyen NJCleaner és mentsd el a NJCleaner.py-ba. Ebben a fájlban csak ez az osztály legyen.
+     2.2 Konsturktorban kapja meg a csv elérési útvonalát és olvassa be pandas segítségével és mentsük el a data (self.data) osztályszintű változóba 
+     2.3 Írj egy függvényt ami sorbarendezi a dataframe-et 'scheduled_time' szerint növekvőbe és visszatér a sorbarendezett df-el, a függvény neve legyen 'order_by_scheduled_time' és térjen vissza a df-el  
+     2.4 Dobjuk el a from és a to oszlopokat, illetve azokat a sorokat ahol van nan és adjuk vissza a df-et. A függvény neve legyen 'drop_columns_and_nan' és térjen vissza a df-el  
+     2.5 A date-et alakítsd át napokra, pl.: 2018-03-01 --> Thursday, ennek az oszlopnak legyen neve a 'day'. Ezután dobd el a 'date' oszlopot és térjen vissza a df-el. A függvény neve legyen 'convert_date_to_day' és térjen vissza a df-el   
+     2.6 Hozz létre egy új oszlopot 'part_of_the_day' névvel. A 'scheduled_time' oszlopból számítsd ki az alábbi értékeit. A 'scheduled_time'-ot dobd el. A függvény neve legyen 'convert_scheduled_time_to_part_of_the_day' és térjen vissza a df-el  
          4:00-7:59 -- early_morning  
          8:00-11:59 -- morning  
          12:00-15:59 -- afternoon  
          16:00-19:59 -- evening  
          20:00-23:59 -- night  
          0:00-3:59 -- late_night  
-    - A késéeket jelöld az alábbiak szerint. Az új osztlop neve legyen 'delay'. A függvény neve legyen pedig 'convert_delay' és térjen vissza a df-el
-         0 <= x 5  --> 0  
-         5 <= x    --> 1  
-    - Dobd el a felesleges oszlopokat 'train_id' 'scheduled_time' 'actual_time' 'delay_minutes'. A függvény neve legyen 'drop_unnecessary_columns' és térjen vissza a df-el
-    - Írj egy olyan metódust, ami elmenti a dataframe első 60 000 sorát. A függvénynek egy string paramétere legyen, az pedig az, hogy hova mentse el a csv-t (pl.: 'data/NJ.csv'). A függvény neve legyen 'save_first_60k'. 
-    - Írj egy függvényt ami a fenti függvényeket összefogja és megvalósítja (sorbarendezés --> drop_columns_and_nan --> ... --> save_first_60k), a függvény neve legyen 'prep_df'. Egy paramnétert várjon, az pedig a csv-nek a mentési útvonala legyen. Ha default value-ja legyen 'data/NJ.csv'
+    2.7 A késéseket jelöld az alábbiak szerint. Az új osztlop neve legyen 'delay'. A függvény neve legyen pedig 'convert_delay' és térjen vissza a df-el
+         0min <= x < 5min   --> 0  
+         5min <= x          --> 1  
+    2.8 Dobd el a felesleges oszlopokat 'train_id' 'scheduled_time' 'actual_time' 'delay_minutes'. A függvény neve legyen 'drop_unnecessary_columns' és térjen vissza a df-el
+    2.9 Írj egy olyan metódust, ami elmenti a dataframe első 60 000 sorát. A függvénynek egy string paramétere legyen, az pedig az, hogy hova mentse el a csv-t (pl.: 'data/NJ.csv'). A függvény neve legyen 'save_first_60k'. 
+    2.10 Írj egy függvényt ami a fenti függvényeket összefogja és megvalósítja (sorbarendezés --> drop_columns_and_nan --> ... --> save_first_60k), a függvény neve legyen 'prep_df'. Egy paramnétert várjon, az pedig a csv-nek a mentési útvonala legyen. Ha default value-ja legyen 'data/NJ.csv'
 
 3.  A feladatot a HAZI06.py-ban old meg.
     Az órán megírt DecisionTreeClassifier-t fit-eld fel az első feladatban lementett csv-re. 
-    A feladat célja az, hogy határozzuk meg azt, hogy a vonatok késnek-e vagy sem. 0p <= x < 5p --> nem késik, ha 5 < x --> késik.
+    A feladat célja az, hogy határozzuk meg azt, hogy a vonatok késnek-e vagy sem. 0p <= x < 5p --> nem késik (0), ha 5p <= x --> késik (1).
     Az adatoknak a 20% legyen test és a splitelés random_state-je pedig 41 (mint órán)
     A testset-en 80% kell elérni. Ha megvan a minimum százalék, akkor azzal paraméterezd fel a decisiontree-t és azt kell leadni.
 
@@ -35,9 +37,10 @@
 4.  A tanításodat foglald össze 4-5 mondatban a HAZI06.py-ban a fájl legalján kommentben. Írd le a nehézségeket, mivel próbálkoztál, mi vált be és mi nem. Ezen kívül írd le 10 fitelésed eredményét is, hogy milyen paraméterekkel probáltad és milyen accuracy-t értél el. 
 Ha ezt feladatot hiányzik, akkor nem fogadjuk el a házit!
 
-HAZI06-
-    -NJCleaner.py
-    -HAZI06.py
+HAZI-
+    HAZI06-
+        -NJCleaner.py
+        -HAZI06.py
 
 ##################################################################
 ##                                                              ##
@@ -48,3 +51,61 @@ HAZI06-
 ##                                                              ##
 ##################################################################
 """
+
+
+from sklearn.model_selection import train_test_split
+from NJCleaner import NJCleaner
+from DecisionTreeClassifier  import DecisionTreeClassifier
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+
+nj = NJCleaner('./2018_03.csv')
+nj.prep_df()
+
+
+col_name = ['stop_sequence' , 'from_id' , 'to_id' , 'status' , 'line' , 'type' , 'day', 'part_of_the_day' , 'delay']
+data = pd.read_csv('./data/NJ.csv',skiprows=1, header=None, names=col_name)
+
+
+X = data.iloc[:, :-1].values
+Y = data.iloc[:, -1].values.reshape(-1,1)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=.2, random_state=41)
+
+
+classifier = DecisionTreeClassifier(min_samples_split=2, max_depth=4)
+classifier.fit(X_train, Y_train)
+
+Y_pred = classifier.predict(X_test)
+print(accuracy_score(Y_test, Y_pred))
+
+
+
+
+#A feladat alapján megírtam az NJCleaner.py fájlt és NJCleaner osztályt. Majd a HAZI06.ipynb-ben tesztelem, ahol
+#kisebb bugokba futottam. Egy VS Code reload megoldotta a problémát. Utána az órai feladat alapján felparamétereztem a decision tree-t.
+#Pár tesztelés után sikerült elérni egy 90%-os accuracy-t.
+
+#Nehézségek:
+#Nem találta a prep_df() metódust (pedig ott volt)
+#Nem találta meg NJ.csv fájlt (pedig ott volt)
+
+
+# min_samples_split=2, max_depth=4
+#Eredmények:
+#1  min_samples_split=1, max_depth=1 --> accuracy: 0.7773333333333333
+#2  min_samples_split=1, max_depth=2 --> accuracy: 0.7823333333333333
+#3  min_samples_split=1, max_depth=3 --> accuracy: 0.7839166666666667
+#4  min_samples_split=1, max_depth=4 --> accuracy: 0.7849166666666667
+#5  min_samples_split=2, max_depth=1 --> accuracy: 0.7773333333333333
+#6  min_samples_split=2, max_depth=2 --> accuracy: 0.7823333333333333
+#7  min_samples_split=2, max_depth=3 --> accuracy: 0.7839166666666667
+#8  min_samples_split=2, max_depth=4 --> accuracy: 0.7849166666666667
+#9  min_samples_split=3, max_depth=1 --> accuracy: 0.7773333333333333
+#10 min_samples_split=3, max_depth=2 --> accuracy: 0.7823333333333333
+#11 min_samples_split=2, max_depth=6 --> accuracy: 0.7885
+#12 min_samples_split=4, max_depth=6 --> accuracy: 0.7885
+#13 min_samples_split=5, max_depth=5 --> accuracy: 0.7885833333333333
+#14 min_samples_split=2, max_depth=7 --> accuracy: 0.7934166666666667
+#14 min_samples_split=7, max_depth=7 --> accuracy: 0.7935
+
--- HAZI/HAZI06/NJCleaner.py
+++ HAZI/HAZI06/NJCleaner.py
@@ -1,65 +1,59 @@
 import pandas as pd
-import numpy as np
 
 class NJCleaner:
-    def __init__(self, path) -> None:
-        self.path = path
-        self.data = pd.read_csv(path)
     
-    def order_by_scheduled_time(self) -> pd.DataFrame:
-        order = self.data.sort_values(by=['scheduled_time'])
-        return order
-
-
-    def grep_df(self,save_csv_path):
-        self.data = self.order_by_scheduled_time()
-        self.data.to_csv(save_csv_path)
-
-    def drop_columns_and_nan(in_df : pd.DataFrame) -> pd.DataFrame:
-        out_df = in_df.copy()
-        out_df.drop('from','to')
-        out_df.dropna(axis=0)
-        return out_df
+    def __init__(self, csv_path):
+        self.data = pd.read_csv(csv_path)
     
-    def convert_date_to_day(in_df : pd.DataFrame) -> pd.DataFrame:
-        out_df = in_df.copy()
-        out_df['day'] = pd.to_datetime(out_df['date']).dt.day_name()
-        return out_df
-
-    def convert_scheduled_time_to_part_of_the_day(in_df : pd.DataFrame) -> pd.DataFrame:
-        out_df = in_df.copy()
-        out_df['part_of_the_day'] = ''
-        for i in range(len(out_df)):
-            scheduledtime = out_df['scheduled_time'][i]
-            if scheduledtime < '4:00':
-                out_df['part_of_the_day'][i] = 'late_night'
-                continue
-            if scheduledtime < '8:00':
-                out_df['part_of_the_day'][i] = 'early_morning'
-                continue
-            if scheduledtime < '12:00':
-                out_df['part_of_the_day'][i] = 'morning'
-                continue
-            if scheduledtime < '16:00':
-                out_df['part_of_the_day'][i] = 'afternoon'
-                continue
-            if scheduledtime < '20:00':
-                out_df['part_of_the_day'][i] = 'evening'
-                continue
-            if scheduledtime <= '23:59':
-                out_df['part_of_the_day'][i] = 'late_night'
-                continue
-        return out_df
+    def order_by_scheduled_time(self):
+        self.data = self.data.sort_values(by=['scheduled_time'])
+        return self.data
+    
+    def drop_columns_and_nan(self):
+        self.data = self.data.dropna()
+        self.data = self.data.drop(['from', 'to'], axis=1)
+        return self.data
+    
+    def convert_date_to_day(self):
+        self.data['day'] = pd.to_datetime(self.data['date']).dt.day_name()
+        self.data = self.data.drop(['date'], axis=1)
+        return self.data
+    
+    def convert_scheduled_time_to_part_of_the_day(self):
+        def get_part_of_day(hour):
+            if 4 <= hour < 8:
+                return 'early_morning'
+            elif 8 <= hour < 12:
+                return 'morning'
+            elif 12 <= hour < 16:
+                return 'afternoon'
+            elif 16 <= hour < 20:
+                return 'evening'
+            elif 20 <= hour <= 23:
+                return 'night'
+            else:
+                return 'late_night'
+        
+        self.data['part_of_the_day'] = pd.to_datetime(self.data['scheduled_time']).dt.hour.apply(get_part_of_day)
+        self.data = self.data.drop(['scheduled_time'], axis=1)
+        return self.data
     
     def convert_delay(self):
-        out_df = self.data.copy()
-        out_df['delay'] = out_df['delay_minutes'].apply(get_delay)
-
-        def get_delay(delay):
-            if delay>=5:
-                return '1'
-            return '0'
-        
-        return out_df
+        self.data['delay'] = self.data['delay_minutes'].apply(lambda x: 1 if x >= 5 else 0)
+        return self.data
     
+    def drop_unnecessary_columns(self):
+        self.data = self.data.drop(['train_id', 'actual_time', 'delay_minutes'], axis=1)
+        return self.data
     
+    def save_first_60k(self, path):
+        self.data.iloc[:60000].to_csv(path, index=False)
+    
+    def prep_df(self, path='data/NJ.csv'):
+        self.order_by_scheduled_time()
+        self.drop_columns_and_nan()
+        self.convert_date_to_day()
+        self.convert_scheduled_time_to_part_of_the_day()
+        self.convert_delay()
+        self.drop_unnecessary_columns()
+        self.save_first_60k(path)
