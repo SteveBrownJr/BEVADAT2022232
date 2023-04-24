bde55648ac71f3053cd5e39036d6e7733d79277a
Steve_Brown <barnapisti19671@gmail.com>
2023-04-24 18:39:24 +0200

    Update LinearRegressionSkeleton.py

--- GYAK/GYAK08/LinearRegressionSkeleton.py
+++ GYAK/GYAK08/LinearRegressionSkeleton.py
@@ -1,26 +1,37 @@
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import matplotlib.pyplot as plt
 
 
 class LinearRegression:
     def __init__(self, epochs: int = 1000, lr: float = 1e-3):
         self.epochs = epochs
         self.lr = lr
+        self.m = 0
+        self.c = 0
+        self.losses = []
+
 
     def fit(self, X: np.array, y: np.array):
-        n_samples = X.shape[0]
-        n_features = X.shape[1]
-
-        self.weights = np.zeros(n_features + 1)
-
-        X = np.insert(X, 0, 1, axis=1)
-
-        for _ in range(self.epochs):
-            y_pred = np.dot(X, self.weights)
-            error = y - y_pred
-            gradient = -2 * np.dot(X.T, error) / n_samples
-            self.weights -= self.lr * gradient
+        n = float(len(X)) 
         
+        for i in range(self.epochs): 
+            y_pred = self.m * X + self.c
+
+            residuals = y - y_pred
+            loss = np.sum(residuals ** 2)
+            self.losses.append(loss)
+            D_m = (-2/n) * sum(X * residuals)
+            D_c = (-2/n) * sum(residuals)
+            self.m = self.m - self.lr * D_m 
+            self.c = self.c - self.lr * D_c
+        return self.losses
+
     def predict(self, X):
-        X = np.insert(X, 0, 1, axis=1)
-        y_pred = np.dot(X, self.weights)
-        return y_pred
+        return self.m * X + self.c
+    
+    def evaluate(self, X, y):
+        pred = self.predict(X)
+        return np.mean((pred - y)**2)
